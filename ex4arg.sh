7398014e778586dd4e0a038e8645602d568c8637
bbeleta <93370708+bbeleta@users.noreply.github.com>
2021-11-10 10:36:14 +0100

    Add files via upload

---
+++ ex4arg.sh
@@ -0,0 +1,9 @@
+#exercici arguments 4
+
+
+echo El primer argument és $1
+echo El segon és $2
+echo el nom del script és $0
+
+let suma=$1+$2
+echo "S'han sumat els arguments i el resultat és $suma"
