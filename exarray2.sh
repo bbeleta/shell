0c0653968da6ff7a895f65c64578f7c52e3a4e95
bbeleta <93370708+bbeleta@users.noreply.github.com>
2021-11-11 12:04:04 +0100

    Add files via upload

---
+++ exarray2.sh
@@ -0,0 +1,14 @@
+#Ex array 2
+
+LLETRES=("h" "h" "t" "u" "k" "g" "f" "h" "u" "i")
+
+conta=0
+
+for var in ${LLETRES[@]}
+do
+    if [[ $var == "h" ]] || [[ $var == "H" ]]
+        then
+        conta=$(($conta+1))
+        fi
+done
+echo "Elnombre de "h" és $conta"
