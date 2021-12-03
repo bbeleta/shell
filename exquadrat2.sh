5d136208891c5ac2efd57c8fb73f55a6e2a456b4
bbeleta <93370708+bbeleta@users.noreply.github.com>
2021-12-03 09:46:46 +0100

    Add files via upload

---
+++ exquadrat2.sh
@@ -0,0 +1,24 @@
+#Ex quadrat 2
+#Bel Santandreu Nadal
+
+echo "Digues un numero"
+read numero
+a=0 
+num1=1
+suma=0
+
+for (( i=0; i<numero; i++ ))
+do
+    for (( j=1; j<numero; j++ ))
+    do
+        printf $suma
+    if [ $suma -lt 9 ]
+        then 
+        let suma=$suma+$num1
+    else 
+    suma=0
+fi 
+    done
+    echo $suma
+done
+
